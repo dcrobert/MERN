0d5e3811148cce921fce82a6b9463b9a4e9eeb24
Robertson, Dalton <daltoncrobertson@gmail.com>
2017-08-24 15:11:07 -0500

    finishing up auth flow

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+// googleClientID: "351763962438-neoncosthnd9fhch36i2jcbd6t13hqvj.apps.googleusercontent.com",
+// googleClientSecret: "E2Y6nQD4_RaFRF3_Jk4OMHUC",
+// mongoURI: "mongodb://prodMern:password@ds139448.mlab.com:39448/email-prod",
+// cookieKey: "dsfjhjkho#@$jkj#$%dfg798345#$%?#$%#@%345dfgsdgd"
